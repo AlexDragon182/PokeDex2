f2205b3736d5bc59548c590058546ed521e1fc92
alex_ <alex_182_sp@outlook.com>
2024-12-15 21:39:19 -0600

    PokeDex for Learning good Practices and Dagger

--- app/src/main/java/com/example/my_pokedex/features/pokedex/presentation/PokedexViewModel.kt
+++ app/src/main/java/com/example/my_pokedex/features/pokedex/presentation/PokedexViewModel.kt
@@ -25,11 +25,11 @@ class PokedexViewModel @Inject constructor(
     val response = _response.receiveAsFlow()
     private var searchJob: Job? = null
 
-    val searchQuery: String = ""
+    var searchQuery: String = ""
     var pokemonList: MutableList<PokemonResultModel> = mutableListOf()
-    var noMoreItems : Boolean = false
-    var offset : Int = 0
-    var limit : Int = 40
+    var noMoreItems: Boolean = false
+    var offset: Int = 0
+    var limit: Int = 40
 
 
     init {
@@ -47,7 +47,7 @@ class PokedexViewModel @Inject constructor(
                 is Resource.Success -> {
                     result.data?.let {
                         if (it.results.isEmpty()) {
-                           noMoreItems = true
+                            noMoreItems = true
                         } else {
                             pokemonList.addAll(it.results)
                             offset += 40
@@ -66,4 +66,45 @@ class PokedexViewModel @Inject constructor(
             }
         }
     }
+
+
+    fun getPokemonByName() {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            _response.send(Loading())
+            pokemonList.clear()
+            offset = 0
+            if (searchQuery.isEmpty()) {
+                getPokemon()
+            }
+            else {
+                val result = repository.getPokemonByName(searchQuery)
+                when (result) {
+                    is Resource.Success -> {
+                        println(".getPokemonByName SUCCESS")
+                        result.data?.let {
+                            it.name?.let {
+                                pokemonList.add(PokemonResultModel(it))
+                                _response.send(Success(data = it))
+                            }
+                        }
+                    }
+
+                    is Resource.Error -> {
+                        println(".getPokemonByName ERROR")
+                        _response.send(Error())
+                    }
+
+                    is Resource.Loading -> {
+                        //NO-OP//
+                    }
+                }
+            }
+        }
+    }
+
+    fun setQuery(myQuery: String){
+        searchQuery = myQuery
+    }
+
 }
