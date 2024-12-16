ebeb854fdb5dc82bd73588da0e8237fc9ed76154
alex_ <alex_182_sp@outlook.com>
2024-12-15 21:39:19 -0600

    PokeDex for Learning good Practices and Dagger

--- app/src/main/java/com/example/my_pokedex/features/pokedex/presentation/PokedexScreen.kt
+++ app/src/main/java/com/example/my_pokedex/features/pokedex/presentation/PokedexScreen.kt
@@ -1,6 +1,9 @@
 package com.example.my_pokedex.features.pokedex.presentation
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +16,9 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.my_pokedex.R
 import com.example.my_pokedex.databinding.FragmentPokedexScreenBinding
+import com.example.my_pokedex.features.PokemonDetails.domain.PokemonDetailsModel
 import com.example.my_pokedex.features.pokedex.domain.PokemonModel
+import com.example.my_pokedex.features.pokedex.domain.PokemonResultModel
 import com.example.my_pokedex.networking.ApiPokedex
 import com.example.my_pokedex.utils.Error
 import com.example.my_pokedex.utils.Loading
@@ -47,7 +52,7 @@ class PokedexScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPokedexScreenBinding.inflate(inflater,container,false)
+        _binding = FragmentPokedexScreenBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -57,34 +62,62 @@ class PokedexScreen : Fragment() {
         binding.rvPokemon.adapter = pokedexAdapter
         registerObservers()
         addScrollListener()
+        setListeners()
+        if (viewModel.pokemonList.isEmpty()) {
+            viewModel.getPokemon()
+        } else {
+            pokedexAdapter.submitList(viewModel.pokemonList)
+            binding.rvPokemon.scheduleLayoutAnimation()
+        }
+    }
+
+    private fun setListeners() {
+        binding.searchPokemonText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                s!!
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s!!
+                viewModel.setQuery(s.toString())
+            }
+            override fun afterTextChanged(s: Editable?) {
+                s!!
+            }
+        })
+
+        binding.searchButton.setOnClickListener{
+            viewModel.getPokemonByName()
+        }
+
+
     }
 
     private fun addScrollListener() {
-            binding.rvPokemon.addOnScrollListener(object :
-                RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
+        binding.rvPokemon.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
 
-                    if (dy > 0) {
-                        binding.rvPokemon.layoutManager?.let {
-                            currentItem = it.childCount
-                            totalItemCount = it.itemCount
-                            lastVisibleItem = (it as LinearLayoutManager).findFirstVisibleItemPosition()
-                            if (!isLoading && (currentItem + lastVisibleItem == totalItemCount) && currentLastItemId != previousLastItemId) {
-                                isLoading = true
-                                previousLastVisibleItemCache = previousLastVisibleItem
-                                if (viewModel.pokemonList.size > 0) {
-                                    previousLastVisibleItem = viewModel.pokemonList.size - 1
-                                    previousLastItemId =
-                                        viewModel.pokemonList.get(viewModel.pokemonList.size - 1).name
-                                }
-                                viewModel.getPokemon()
+                if (dy > 0) {
+                    binding.rvPokemon.layoutManager?.let {
+                        currentItem = it.childCount
+                        totalItemCount = it.itemCount
+                        lastVisibleItem = (it as LinearLayoutManager).findFirstVisibleItemPosition()
+                        if (!isLoading && (currentItem + lastVisibleItem == totalItemCount) && currentLastItemId != previousLastItemId) {
+                            isLoading = true
+                            previousLastVisibleItemCache = previousLastVisibleItem
+                            if (viewModel.pokemonList.size > 0) {
+                                previousLastVisibleItem = viewModel.pokemonList.size - 1
+                                previousLastItemId =
+                                    viewModel.pokemonList.get(viewModel.pokemonList.size - 1).name
                             }
+                            viewModel.getPokemon()
                         }
                     }
                 }
-            })
-        }
+            }
+        })
+    }
 
     private fun registerObservers() {
         lifecycleScope.launch {
@@ -94,7 +127,7 @@ class PokedexScreen : Fragment() {
         }
     }
 
-    private fun handleViewState(viewState: ViewState<Any>){
+    private fun handleViewState(viewState: ViewState<Any>) {
         when (viewState) {
             is Success -> {
                 isLoading = false
@@ -102,10 +135,14 @@ class PokedexScreen : Fragment() {
                 if (viewState.data is PokemonModel) {
                     val response = viewState.data.results
                     pokedexAdapter.submitList(viewModel.pokemonList)
-                    if (isFirstTimeLoading){
+                    if (isFirstTimeLoading) {
                         binding.rvPokemon.scheduleLayoutAnimation()
                         isFirstTimeLoading = false
                     }
+                } else if (viewState.data is PokemonDetailsModel) {
+                    val response = viewState.data.name
+                    pokedexAdapter.submitList(viewModel.pokemonList)
+                    binding.rvPokemon.scheduleLayoutAnimation()
                 }
             }
 
